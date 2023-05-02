619d60618a9968db4a3f927e532e3fa11be36ecc
mudit102 <111658006+mudit102@users.noreply.github.com>
2023-05-02 10:18:57 +0530

    Time: 0 ms (100.00%), Space: 42.3 MB (38.04%) - LeetHub

--- 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.java
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.java
@@ -1,12 +1,12 @@
 class Solution {
     public int arraySign(int[] nums) {
-        int sign = 1;
+        int sign = 1;//make one sign variable which is product
         
-        for (int num : nums) {
+        for (int num : nums) {//iteate array elements
             if (num == 0) {
                 return 0;
             } else if (num < 0) {
-                sign = -sign;
+                sign = -sign;//if neg flip sign
             }
         }
         
