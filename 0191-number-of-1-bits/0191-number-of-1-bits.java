bd648097791738489315dfd41e3b58c1864e6b51
Mudit Saxena <111658006+mudit102@users.noreply.github.com>
2023-11-30 00:33:42 +0530

    Time: 0 ms (100.00%), Space: 39.4 MB (44.26%) - LeetHub

---
+++ 0191-number-of-1-bits/0191-number-of-1-bits.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public int hammingWeight(int n) {
+        return Integer.bitCount(n);
+    }
+
+    // Example usage:
+    // public static void main(String[] args) {
+    //     Solution solution = new Solution();
+    //     int result = solution.hammingWeight(0b110101);
+    //     System.out.println(result);
+    // }
+}
