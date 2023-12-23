8194ec0ed98b826f32b1b616ca3775dcfa87d884
Mudit Saxena <111658006+mudit102@users.noreply.github.com>
2023-12-24 00:40:21 +0530

    Time: 3 ms (51.76%), Space: 41.5 MB (14.32%) - LeetHub

---
+++ 1496-path-crossing/1496-path-crossing.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isPathCrossing(String path) {
+        int n = path.length();
+        int x = 0;
+        int y = 0;
+        for(int i=0;i<n;i++) {
+            int tempX = x;
+            int tempY = y;
+            for(int j=i;j<n;j++) {
+                char ch = path.charAt(j);
+                if(ch == 'N') y++;
+                else if(ch == 'S') y--;
+                else if(ch == 'W') x--;
+                else x++;
+                if(x == tempX && y == tempY) return true;
+            }
+        }
+        return false;
+    }
+}
